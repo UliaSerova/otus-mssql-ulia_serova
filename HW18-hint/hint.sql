6de6fa29b17a7caa696eaf6eb8c09a71e0fa41c9
ulia <ulia-2012@list.ru>
2026-04-24 20:07:06 +0300

    hint

--- HW18-hint/hint.sql
+++ HW18-hint/hint.sql
@@ -1,25 +1,32 @@
+// с добавлением индексов время выполнения снизилось в 2 раза
+create index OrderID_Invoices on Sales.Invoices(InvoiceID, OrderID, CustomerID, BillToCustomerID, InvoiceDate)
+
+create NONCLUSTERED index OrderLines 
+on Sales.OrderLines (OrderLineID) INCLUDE(OrderID, StockItemID, UnitPrice, Quantity)
+
+create index InvoiceID_CustomerTransactions on Sales.CustomerTransactions(CustomerTransactionID , InvoiceID)
+
 SET STATISTICS io, time on;
---create index StockItemID_OrderLines on Sales.OrderLines(StockItemID, UnitPrice, Quantity)
---create index StockItemID_OrderLines on Sales.OrderLines(StockItemID, OrderID, UnitPrice, Quantity)
+
 Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)    
-FROM Sales.Orders AS ord
+FROM Sales.Orders AS ord 
     JOIN Sales.OrderLines AS det
         ON det.OrderID = ord.OrderID
     JOIN Sales.Invoices AS Inv 
         ON Inv.OrderID = ord.OrderID
-    JOIN Sales.CustomerTransactions AS Trans
+    JOIN Sales.CustomerTransactions AS Trans 
         ON Trans.InvoiceID = Inv.InvoiceID
-    JOIN Warehouse.StockItemTransactions AS ItemTrans
+    JOIN Warehouse.StockItemTransactions AS ItemTrans WITH (HOLDLOCK)
         ON ItemTrans.StockItemID = det.StockItemID
 WHERE Inv.BillToCustomerID != ord.CustomerID
     AND (Select SupplierId
-         FROM Warehouse.StockItems AS It
+         FROM Warehouse.StockItems AS It 
          Where It.StockItemID = det.StockItemID) = 12
-    --AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
-     --   FROM Sales.OrderLines AS Total
-    --        Join Sales.Orders AS ordTotal
-     --           On ordTotal.OrderID = Total.OrderID
-     --   WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
+    AND (SELECT SUM(Total.UnitPrice*Total.Quantity) 
+        FROM Sales.OrderLines AS Total 
+            Join Sales.Orders AS ordTotal
+                On ordTotal.OrderID = Total.OrderID
+        WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
     AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
 GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
